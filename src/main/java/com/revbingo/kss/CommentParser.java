e30f81b69360251c99af48bc5d32322ade518fe6
Mark Piper <mark.piper@wds.co>
2013-10-01 23:10:26 +0100

    MP: Finish CommentParser

--- src/main/java/com/revbingo/kss/CommentParser.java
+++ src/main/java/com/revbingo/kss/CommentParser.java
@@ -1,6 +1,128 @@
 package com.revbingo.kss;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CommentParser {
+
+	private File fileToRead;
+	private String stringToRead;
+	private ArrayList<String> blocks;
+	private boolean parsed;
+	private boolean preserveWhitespace = false;
+	
+	public CommentParser(String filePathOrStringInput) {
+		File file = new File(filePathOrStringInput);
+		if(file.exists()) {
+			fileToRead = file;
+		} else {
+			stringToRead = filePathOrStringInput;
+		}
+		
+		blocks = new ArrayList<String>();
+		parsed = false;
+	}
+	
+	public void preserveWhitespace(boolean preserve) {
+		preserveWhitespace = preserve;
+	}
+
+	public ArrayList<String> blocks() {
+		if(!parsed) {
+			blocks = parseBlocks();
+		}
+		return blocks;
+	}
+	
+	private ArrayList<String> parseBlocks() {
+		String input;
+		if(fileToRead != null) {
+			input = readFile();
+		} else {
+			input = stringToRead;
+		}
+		return parseBlocksInput(input);
+	}
+
+	private ArrayList<String> parseBlocksInput(String input) {
+		String currentBlock = "";
+		boolean inside_single_line_block = false;
+		boolean inside_multi_line_block = false;
+		
+		for(String line : input.split("\n")) {
+			if(isSingleLineComment(line)) {
+				String parsed = parseSingleLine(line);
+				if(inside_single_line_block) {
+					currentBlock += "\n" + parsed;
+				} else {
+					currentBlock = parsed;
+					inside_single_line_block = true;
+				}
+			}
+			
+			if(isStartOfMultiLineComment(line) || inside_multi_line_block) {
+				String parsed = parseMultiLine(line);
+				if(inside_multi_line_block) {
+					currentBlock += "\n" + parsed;
+				} else {
+					currentBlock = parsed;
+					inside_multi_line_block = true;
+				}
+			}
+			
+			if(isEndOfMultiLineComment(line)) {
+				inside_multi_line_block = false;
+			}
+			
+			if(!isSingleLineComment(line) && !inside_multi_line_block) {
+				if(currentBlock != null) {
+					blocks.add(normalize(currentBlock));
+				}
+				
+				inside_single_line_block = false;
+				currentBlock = null;
+			}
+		}
+		this.parsed = true;
+		return blocks;
+	}
+
+	private String normalize(String currentBlock) {
+		if(preserveWhitespace) return currentBlock;
+		
+		currentBlock = currentBlock.replaceAll("(?m)^\\s*\\*", "");
+		
+		Integer indentSize = null;
+		StringBuffer unindented = new StringBuffer();
+		Pattern precedingWhitespacePattern = Pattern.compile("^\\s*");
+		for(String piece : currentBlock.split("\n")) {
+			int precedingWhitespace = 0;
+			Matcher m = precedingWhitespacePattern.matcher(piece);
+			String match = "";
+			if(m.find()) {
+				match = m.group();
+			}
+			precedingWhitespace = match.length();
+			if(indentSize == null) {
+				indentSize = precedingWhitespace;
+			}
+			
+			if(!piece.equals("")) {
+				if(indentSize <= precedingWhitespace && indentSize > 0) {
+					unindented.append(piece.substring(indentSize));
+				} else {
+					unindented.append(piece);
+				}
+			}
+			unindented.append("\n");
+		}
+		return unindented.toString().trim();
+	}
 
 	public static Boolean isSingleLineComment(String line) {
 		return line.matches("^\\s*//.*");
@@ -15,10 +137,36 @@ public class CommentParser {
 	}
 
 	public static String parseSingleLine(String line) {
-		return line.replaceFirst("\\s*//", "").trim();
+		return rstrip(line.replaceFirst("\\s*//", ""));
 	}
 
 	public static String parseMultiLine(String line) {
-		return line.replaceFirst("\\s*/\\*", "").replaceFirst("\\*/", "").trim();
+		return rstrip(line.replaceFirst("\\s*/\\*", "").replaceFirst("\\*/", ""));
+	}
+
+	private String readFile() {
+		BufferedReader br = null;
+		StringBuffer stringInput = null;
+		try {
+			br = new BufferedReader(new FileReader(fileToRead));
+			stringInput = new StringBuffer();
+			String s = "";
+			while((s = br.readLine()) != null) {
+				stringInput.append(s);
+				stringInput.append("\n");
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} finally {
+			try {
+				br.close();
+			} catch (IOException e) {}
+		}
+		
+		return stringInput.toString();
+	}
+	
+	private static String rstrip(String input) {
+		return input.replaceAll("\\s*$", "");
 	}
 }
--- src/test/java/com/revbingo/kss/CommentParserTest.java
+++ src/test/java/com/revbingo/kss/CommentParserTest.java
@@ -3,10 +3,22 @@ package com.revbingo.kss;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class CommentParserTest {
 
+	private ArrayList<String> parsedComments;
+	
+	@Before
+	public void loadFixtures() {
+		String filePath = "src/test/resources/comments.txt";
+		CommentParser unit = new CommentParser(filePath);
+		parsedComments = unit.blocks();
+	}
+	
 	@Test
 	public void detectsSingleLineSyntax() throws Exception {
 		assertThat(CommentParser.isSingleLineComment("// yuuuuup"), is(true));
@@ -27,55 +39,42 @@ public class CommentParserTest {
 
 	@Test
 	public void parsesSingleLineCommentSyntax() throws Exception {
-		assertThat(CommentParser.parseSingleLine("// yuuuuup"), is("yuuuuup"));
+		assertThat(CommentParser.parseSingleLine("// yuuuuup"), is(" yuuuuup"));
 	}
 
 	@Test
 	public void parsesMultiLineCommentSyntax() throws Exception {
-		assertThat(CommentParser.parseMultiLine("/* yuuuuup */"), is("yuuuuup"));
+		assertThat(CommentParser.parseMultiLine("/* yuuuuup */"), is(" yuuuuup"));
 	}
 	
 	@Test
 	public void findsSingleLineCommentStyles() throws Exception {
+		String expectedComment = "This comment block has comment identifiers on every line.\n\nFun fact: this is Kyle's favorite comment syntax!";
 		
+		assertThat(parsedComments.contains(expectedComment), is(true));
 	}
-//	  test "finds single-line comment styles" do
-//	    expected  = <<comment
-//	This comment block has comment identifiers on every line.
-//
-//	Fun fact: this is Kyle's favorite comment syntax!
-//	comment
-//	    assert @parsed_comments.include? expected.rstrip
-//	  end
-//
-//	  test "finds block-style comment styles" do
-//	        expected  = <<comment
-//	This comment block is a block-style comment syntax.
-//
-//	There's only two identifier across multiple lines.
-//	comment
-//	    assert @parsed_comments.include? expected.rstrip
-//
-//
-//	      expected  = <<comment
-//	This is another common multi-line comment style.
-//
-//	It has stars at the begining of every line.
-//	comment
-//	  assert @parsed_comments.include? expected.rstrip
-//
-//	  end
-//
-//	  test "handles mixed styles" do
-//	    expected = "This comment has a /* comment */ identifier inside of it!"
-//	    assert @parsed_comments.include? expected
-//
-//	    expected = "Look at my //cool// comment art!"
-//	    assert @parsed_comments.include? expected
-//	  end
-//
-//	  test "handles indented comments" do
-//	    assert @parsed_comments.include? "Indented single-line comment."
-//	    assert @parsed_comments.include? "Indented block comment."
-//	  end
+	
+	@Test
+	public void findsBlockStyleCommentStyles() throws Exception {
+		String expectedComment = "This comment block is a block-style comment syntax.\n\nThere's only two identifier across multiple lines.";
+		assertThat(parsedComments.contains(expectedComment), is(true));
+		
+		expectedComment = "This is another common multi-line comment style.\n\nIt has stars at the begining of every line.";
+		assertThat(parsedComments.contains(expectedComment), is(true));
+	}
+
+	@Test
+	public void handlesMixedStyles() throws Exception {
+		String expectedComment = "This comment has a /* comment */ identifier inside of it!";
+		assertThat(parsedComments.contains(expectedComment), is(true));
+		
+		expectedComment = "Look at my //cool// comment art!";
+		assertThat(parsedComments.contains(expectedComment), is(true));
+	}
+	
+	@Test
+	public void handlesIndentedComments() throws Exception {
+		assertThat(parsedComments.contains("Indented single-line comment."), is(true));
+		assertThat(parsedComments.contains("Indented block comment."), is(true));
+	}
 }
