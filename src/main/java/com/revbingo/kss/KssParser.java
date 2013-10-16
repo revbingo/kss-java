6d025619154256f4a7be6691aa453d4f47d8ddb4
Mark Piper <mark.piper@wds.co>
2013-10-16 08:06:17 +0100

    MP: Refactor KssParser

--- src/main/java/com/revbingo/kss/KssParser.java
+++ src/main/java/com/revbingo/kss/KssParser.java
@@ -16,20 +16,17 @@ public class KssParser {
 	public static final Pattern STYLEGUIDE_PATTERN = Pattern.compile("(?i)(?<!No )Styleguide [0-9A-Za-z ]+");
 	private Map<String, Section> sections = new HashMap<String, Section>();
 	
-	IOFileFilter cssFilter = new SuffixFileFilter(new String[] {"css", "less", "scss", "sass"});
-	
 	public KssParser(String... pathsOrStrings) {
 		for(String pathOrString : pathsOrStrings) {
 			File f = new File(pathOrString);
 			if(f.exists() && f.isDirectory()) {
-				Iterator<File> fileIterator = FileUtils.iterateFiles(f, cssFilter, null);
+				Iterator<File> fileIterator = FileUtils.iterateFiles(f, new String[] {"css", "less", "scss", "sass"}, true);
 				while(fileIterator.hasNext()) {
 					File cssfile = fileIterator.next();
 					addKssBlocks(cssfile.getAbsolutePath(), cssfile.getName());
 				}
 			} else {
-				String kssString = pathOrString;
-				addKssBlocks(kssString, "");
+				addKssBlocks(pathOrString, "");
 			}
 		}
 	}
